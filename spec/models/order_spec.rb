4aa1609bef3bfc42f3b02c26a70b864a7027abe9
mikekauffman <mike.a.kauffman@gmail.com>
2014-05-12 14:45:45 -0600

    Unit-tested the Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Order do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "allows an order to be created" do
+    order = Order.create!(number: "Happiness15")
+    expect(order.number).to eq ("Happiness15")
+  end
+
+  it "allows an Order to be associated with multiple line-items" do
+    order = Order.create!(number: "Happiness15")
+    LineItem.create!(order_id: order.id, description: "iMac")
+    LineItem.create!(order_id: order.id, description: "AppleTV")
+    line_item_desc_array = order.line_items.map {|line_items| line_items.description}
+    expect(line_item_desc_array).to match_array(["iMac","AppleTV"])
+  end
+
+  it "allows an Order ot be associated with an address" do
+    order = Order.create!(number: "Happiness15")
+    Address.create!(order_id: order.id, line1: '123 Easy Street')
+    expect(order.address.line1).to eq "123 Easy Street"
+  end
 end
